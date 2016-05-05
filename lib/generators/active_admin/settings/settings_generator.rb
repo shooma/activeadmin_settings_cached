cd3f43b360cd9052d7fdb1b4da0ee7f4211f8534
shooma <shooma.mishkov@gmail.com>
2016-05-05 19:35:36 +0500

    fix(naming): rename generator template

--- lib/generators/active_admin/settings/settings_generator.rb
+++ lib/generators/active_admin/settings/settings_generator.rb
@@ -4,7 +4,7 @@ module ActiveadminSettingsCached
       source_root File.expand_path('../templates', __FILE__)
 
       def generate_config_file
-        template 'settings.rb', "app/admin/#{file_path.tr('/', '_')}.rb"
+        template 'site_config.rb', "app/admin/#{file_path.tr('/', '_')}.rb"
       end
     end
   end
